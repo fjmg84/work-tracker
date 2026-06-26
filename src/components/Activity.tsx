53c54ac6c129cdac1221c52cf242ecdb39ab0ad8
fjmg84 <fjmgqba@gmail.com>
2026-06-26 17:46:49 -0500

    Update month selector to display month names instead of numbers

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -99,7 +99,7 @@ export default function Activity({ projects }: ActivityProps) {
           >
             {Array.from({ length: 12 }, (_, i) => (
               <option key={i + 1} value={i + 1}>
-                {i + 1}
+                {new Date(0, i).toLocaleString("es-ES", { month: "long" })}
               </option>
             ))}
           </select>
